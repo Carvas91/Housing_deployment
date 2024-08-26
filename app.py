2ec92c9ed851e4d130a70ee4f414192d58269475
carvas91 <carvas91@hotmail.com>
2024-08-26 13:19:17 -0400

    new commit

--- app.py
+++ app.py
@@ -26,9 +26,11 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
+    print("Received form data:", data)
     final_input=scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
+    print("Prediction output:", output)
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
 
 
